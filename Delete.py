08af2e872a2b4af01c2b18bd1b88b0464844e781
alhariri <19abdul97@gmail.com>
2022-06-28 17:21:07 +0100

    interacting with google maps, sorting our data base, returning stations from our DB

---
+++ Delete.py
@@ -0,0 +1,6 @@
+oo =  None
+
+if oo == None:
+    print(" it's not none")
+else: 
+    print("no it's")
--- database.py
+++ database.py
@@ -1,5 +1,5 @@
 import motor.motor_asyncio
-client = motor.motor_asyncio.AsyncIOMotorClient("mongodb+srv://baldock92:aauab*M6y$PGE8s@cluster0.b0udzos.mongodb.net/?retryWrites=true&w=majority")
+client = motor.motor_asyncio.AsyncIOMotorClient("mongodb+srv://me:mongo@cluster0.ma8gt.mongodb.net/?retryWrites=true&w=majority")
 
 database=client.petrol
 collection = database.stations
@@ -14,23 +14,40 @@ apiKey = os.getenv("googleAPIKey")
 
 import googlemaps
 gmaps = googlemaps.Client(key = apiKey)
+import json
+
 
 async def get_petrol_stations(lat, lon):
+    allTheStaionsId = []
+    newFromGoogle = []
     result = gmaps.places( location=(f"{lat}, {lon}"), radius=2, type="gas_station")
-
     i=0
-    newArr = []
+    # print(type(result))
+    # return result
     while i < len(result["results"]):
-        newArr.append({"name" : result["results"][i]["name"], "station_id" : result["results"][i]["place_id"], "address" : result["results"][i]["formatted_address"], "coordinates" : {"lat" : result["results"][i]["geometry"]["location"]["lat"], "lng" : result["results"][i]["geometry"]["location"]["lng"]}, "price" : 0, "votes" : 0 })
-        i+=1
-    
-    document = {"example" : str}
-    converted = json.dumps({"example": newArr[0]})
-    inserting = await collection.insert_one(document)
-    return converted
-    
-    #conditional logic, check if station_id in database already
+        newFromGoogle.append({ "name" : result["results"][i]["name"], "station_id" : result["results"][i]["place_id"], "address" : result["results"][i]["formatted_address"], "coordinates" : {"lat" : result["results"][i]["geometry"]["location"]["lat"], "lng" : result["results"][i]["geometry"]["location"]["lng"]}, "price" : 0, "votes" : 0 })
+        allTheStaionsId.append(result["results"][i]["place_id"])
+        i += 1
+#####################
+    j= 0
+    while j < len(newFromGoogle):
+        document = await collection.find_one({"station_id" : newFromGoogle[j]["station_id"]})
+        if document == None:
+            newStationToDataBase = await collection.insert_one(newFromGoogle[j])
+        # else:
+        #     print( "It's already in your database")
+        j += 1
+#####################
+    d = 0 
+    staionsFromDataBase = []
+    while d < len(allTheStaionsId):
+        document = await collection.find_one({"station_id" : allTheStaionsId[d]})
+        staionsFromDataBase.append(document)
+        d +=1
+    print(type(staionsFromDataBase))
 
-    
-
-
+    return(staionsFromDataBase)
+    # print(type())
+    # print(type(staionsFromDataBase))
+    # return staionsFromDsataBase
+    # return {"staions":{"d":staionsFromDataBase}, "lon":"any", "lat": "any", "user": "jj"}
